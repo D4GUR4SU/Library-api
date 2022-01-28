e1e6a47cd33dc94f226264c1adbb196afdd01abf
Dagurasu56 <douglas_search@hotmail.com>
2022-01-28 15:13:18 -0300

    renomeando classe de start da aplicação

--- src/main/java/com/dagurasu/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/dagurasu/libraryapi/LibraryApiApplication.java
@@ -1,4 +1,4 @@
-package com.dagurasu;
+package com.dagurasu.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/dagurasu/libraryapi/LibraryApiApplicationTests.java
+++ src/test/java/com/dagurasu/libraryapi/LibraryApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.dagurasu;
+package com.dagurasu.libraryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
