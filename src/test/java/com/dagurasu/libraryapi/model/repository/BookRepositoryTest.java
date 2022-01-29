d05c5a3464bfc0fe2311ec3fb8ddd9f218f550f1
Dagurasu56 <douglas_search@hotmail.com>
2022-01-29 17:19:45 -0300

    verificando a existencia de um livro pelo isbn na base de dados

--- src/test/java/com/dagurasu/libraryapi/model/repository/BookRepositoryTest.java
+++ src/test/java/com/dagurasu/libraryapi/model/repository/BookRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.dagurasu.libraryapi.api.entity.Book;
 import com.dagurasu.libraryapi.api.model.repository.BookRepository;
 
 @DataJpaTest
@@ -25,13 +26,26 @@ public class BookRepositoryTest {
 	BookRepository repository;
 
 	@Test
-	@DisplayName("Deve retornar verdadeiro quando existir um livro com o isbn informado.")
-	public void returnTrueWithIsbnExists() {
+	@DisplayName("Deve retornar verdadeiro quando existir um livro na base com o isbn informado.")
+	public void returnTrueWhenIsbnExists() {
+
+		String isbn = "123";
+		Book book = Book.builder().title("O Mestre do Bug").author("Douglas").isbn(isbn).build();
+		entityManager.persist(book);
+
+		boolean exists = repository.existsByIsbn(isbn);
+
+		assertThat(exists).isTrue();
+	}
+	
+	@Test
+	@DisplayName("Deve retornar falso quando não existir um livro na base com o isbn informado.")
+	public void returnFalseWhenIsbnDoesntExists() {
 
 		String isbn = "123";
 
 		boolean exists = repository.existsByIsbn(isbn);
 
-		assertThat(exists).isTrue();
+		assertThat(exists).isFalse();
 	}
 }
