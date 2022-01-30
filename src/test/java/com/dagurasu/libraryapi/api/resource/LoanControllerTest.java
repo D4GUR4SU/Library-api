511b3de8c196c83eba4d835fe428bc0d1530c265
Dagurasu56 <douglas_search@hotmail.com>
2022-01-30 20:53:11 -0300

    lançando erro quando não encontrar o livro para fazer o empréstimo

--- src/test/java/com/dagurasu/libraryapi/api/resource/LoanControllerTest.java
+++ src/test/java/com/dagurasu/libraryapi/api/resource/LoanControllerTest.java
@@ -1,11 +1,13 @@
 package com.dagurasu.libraryapi.api.resource;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 import java.util.Optional;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -47,7 +49,7 @@ public class LoanControllerTest {
 	private LoanService loanService;
 
 	@Test
-	@DisplayName("Deve realizar o empréstimo")
+	@DisplayName("Deve realizar o empréstimo.")
 	public void createLoanTest() throws Exception {
 
 		LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
@@ -65,6 +67,25 @@ public class LoanControllerTest {
 		mvc.perform(request)
 			.andExpect(status().isCreated())
 			.andExpect(content().string("1"));
+
+	}
+	
+	@Test
+	@DisplayName("Deve retornar erro ao tentar fazer empréstimo de um livro inexistente.")
+	public void invalidIsbnCreateLoanTest() throws Exception {
+
+		LoanDTO dto = LoanDTO.builder().isbn("123").customer("Fulano").build();
+		String json = new ObjectMapper().writeValueAsString(dto);
+
+		BDDMockito.given(bookService.getBookByIsbn("123")).willReturn(Optional.empty());
+
+		MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post(LOAN_API).accept(MediaType.APPLICATION_JSON)
+				.contentType(MediaType.APPLICATION_JSON).content(json);
+
+		mvc.perform(request)
+			.andExpect(status().isBadRequest())
+			.andExpect(jsonPath("errors", Matchers.hasSize(1)))
+			.andExpect(jsonPath("erros[0]").value("Book not found for passed isbn"));
 
 	}
 
