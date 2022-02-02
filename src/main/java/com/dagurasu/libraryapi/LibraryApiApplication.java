c7c80a9a49378a3850b55c10188cd73251f2c1c7
Dagurasu56 <douglas_search@hotmail.com>
2022-02-01 22:10:35 -0300

    testando envio de e-mails via gmail

--- src/main/java/com/dagurasu/libraryapi/LibraryApiApplication.java
+++ src/main/java/com/dagurasu/libraryapi/LibraryApiApplication.java
@@ -1,37 +1,18 @@
 package com.dagurasu.libraryapi;
 
-import java.util.Arrays;
-import java.util.List;
-
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import com.dagurasu.libraryapi.api.service.EmailService;
-
 @EnableScheduling
 @SpringBootApplication
 public class LibraryApiApplication {
 
-	@Autowired
-	private EmailService emailService;
-	
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
-	}
-	
-	@Bean
-	public CommandLineRunner runner() {
-		return args -> {
-			List<String> emails = Arrays.asList("library-api@gmail.com");
-			emailService.sendEmails("Testando serviço de e-mails", emails);
-			System.out.println("E-mail enviado com sucesso!");
-		};
 	}
 	
 	public static void main(String[] args) {
